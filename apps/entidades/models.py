d14e124724469ee0c3a948b4556d7c9d931d8b47
dennys <dennyscorrea58@gmail.com>
2024-03-13 20:20:50 -0400

    organizar models por appp

--- apps/entidades/models.py
+++ apps/entidades/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms import model_to_dict
 
 # zona
 class Zona(models.Model):
@@ -8,76 +9,13 @@ class Zona(models.Model):
 		verbose_name = 'Zona'
 		verbose_name_plural = 'Zonas'
 
-	def __str__(self) -> str:
-		return super().__str__()
-
-	def save(self, *args, **kwargs):
-		self.zona_residencia = (self.zona_residencia).upper()
-		return super(Zona, self).save(*args, **kwargs)
-
-# ubicacion del producto
-class Ubicacion(models.Model):
-	ubicacion = models.CharField(max_length=60, blank=False, null=False)
-
-	class Meta:
-		verbose_name = 'Ubicación'
-		verbose_name_plural = 'Ubicaciones'
-
 	def __str__(self):
-		return f'{self.ubicacion}'
+		return self.zona_residencia
 
-	def save(self, *args, **kwargs):
-		self.ubicacion = (self.ubicacion).upper()
-		return super(Ubicacion, self).save(*args, **kwargs)
-	
-# tipo de insumo
-class Tipo_insumo(models.Model):
-	nombre_tipo_insumo = models.CharField(max_length=60, blank=False, null=False)
+	def toJSON(self):
+		item = model_to_dict(self)
+		return item
 
-	class Meta:
-		verbose_name = 'Tipo de insumo'
-		verbose_name_plural = 'Tipo de insumos'
-
-	def __str__(self) -> str:
-		return super().__str__()
-
-	def save(self, *args, **kwargs):
-		self.nombre_tipo_insumo = (self.nombre_tipo_insumo).upper()
-		return super(Tipo_insumo, self).save(*args, **kwargs)
-
-# lote del producto
-class Lote(models.Model):
-	lote = models.CharField(max_length=60, blank=False, null=False)
-	canti_pro = models.IntegerField(blank=False, null=False)
-	fecha_lote = models.DateField(auto_now=True, blank=False, null=False)
-
-	class Meta:
-		verbose_name = 'Lote'
-		verbose_name_plural = 'Lotes'
-		ordering = ['lote']
-
-	def __str__(self) -> str:
-		return super().__str__()
-
-	def save(self, *args, **kwargs):
-		self.lote = (self.lote).upper()
-		return super(Lote, self).save(*args, **kwargs)
-	
-# laboratorio
-class Laboratorio(models.Model):
-	nombre_laboratorio = models.CharField(max_length=60, blank=False, null=False)
-
-	class Meta:
-		verbose_name = 'Laboratorio'
-		verbose_name_plural = 'Laboratorios'
-
-	def __str__(self) -> str:
-		return super().__str__()
-
-	def save(self, *args, **kwargs):
-		self.nombre_laboratorio = (self.nombre_laboratorio).upper()
-		return super(Laboratorio, self).save(*args, **kwargs)
-	
 # representante (de la comunidad?)
 class Representante(models.Model):
 	nacio_repr = models.CharField(max_length=2, blank=False, null=False)
@@ -129,63 +67,6 @@ class Paciente(models.Model):
 		self.apellido_pac = (self.apellido_pac).upper()
 		return super(Paciente, self).save(*args, **kwargs)
 
-# productos
-class Producto(models.Model):
-	nombre_producto = models.CharField(max_length=50, blank=False, null=False)
-	id_ubicacion = models.ForeignKey(Ubicacion, verbose_name='Ubicación', on_delete=models.PROTECT, blank=False, null=False)
-	id_tipo_insumo = models.ForeignKey(Tipo_insumo, verbose_name='Tipo de insumo', on_delete=models.PROTECT, blank=False, null=False)
-	id_lote = models.ForeignKey(Lote, verbose_name='Lote', on_delete=models.PROTECT, blank=False, null=False)
-	id_laboratorio = models.ForeignKey(Laboratorio, verbose_name='Laboratorio', on_delete=models.PROTECT, blank=False, null=False)
-	limite = models.IntegerField(blank=False, null=False)
-	stock_prod = models.IntegerField(null = False, blank= False)
-
-	class Meta:
-		verbose_name = 'Producto'
-		verbose_name_plural = 'Productos'
-		ordering = ['nombre_producto']
-
-	def __str__(self):
-		return f'{self.nombre_producto}'
-
-	def save(self, *args, **kwargs):
-		self.nombre_producto = (self.nombre_producto).upper()
-		return super(Producto, self).save(*args, **kwargs)
-	
-# solicitud de producto
-class Solicitud(models.Model):
-	fecha_soli = models.DateField(auto_now=True, blank=False, null=False)
-	cod_pac = models.ForeignKey(Paciente, verbose_name='Paciente', on_delete=models.PROTECT, blank=False, null=False)
-	descripcion_soli = models.TextField(blank=False, null=False)
-	id_producto = models.ManyToManyField(Producto, verbose_name='Producto')
-	recipe = models.FileField(upload_to='recipes/', blank=False, null=False)
-	proceso_actual = models.CharField(max_length=50, default='ATENCION AL CLIENTE')
-	estado = models.CharField(max_length=50, default='EN PROCESO', blank=False, null=False)
-	aceptaciones = models.PositiveIntegerField(default=0)
-
-	class Meta:
-		verbose_name = 'Solicitud de Medicamento'
-		verbose_name_plural = 'Solicitud de Medicamentos'
-		ordering = ['fecha_soli']
-
-	def __str__(self):
-		return f'{self.descripcion_soli}'
-
-	def save(self, *args, **kwargs):
-		self.descripcion_soli = (self.descripcion_soli).upper()
-		return super(Solicitud, self).save(*args, **kwargs)
-
-# detalle de la solicitud
-class DetalleSolicitud(models.Model):
-	solicitud = models.ForeignKey('Solicitud', on_delete=models.CASCADE)
-	producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
-	cantidad = models.IntegerField(default=1)
-
-	class Meta:
-		unique_together = ('solicitud', 'producto')  # Asegurar que no haya duplicados
-
-	def __str__(self):
-		return f'{self.solicitud.id_soli} - {self.producto.nombre} - {self.cantidad}'
-
 # empleados
 class Empleado(models.Model):
 	nacio_empl = models.CharField(max_length=2, blank=False, null=False)
@@ -208,43 +89,6 @@ class Empleado(models.Model):
 		self.nombre_empl = (self.nombre_empl).upper()
 		self.apellido_empl = (self.apellido_empl).upper()
 		return super(Empleado, self).save(*args, **kwargs)
-	
-# tipo de movimiento
-class Tipo_mov(models.Model):
-	descripcion = models.TextField(blank=False, null=False)
-
-	class Meta:
-		verbose_name = 'Tipo de Movimiento'
-		verbose_name_plural = 'Tipos de Movimientos'
-		ordering = ['descripcion']
-
-	def __str__(self):
-		return f'{self.descripcion}'
-
-	def save(self, *args, **kwargs):
-		self.descripcion = (self.descripcion).upper()
-		return super(Tipo_mov, self).save(*args, **kwargs)
-
-# movimiento del inventario
-class Movimiento_inventario(models.Model):
-	fecha_mov = models.DateField(auto_now=True, blank=False, null=False)
-	motivo_mov = models.TextField(blank=False, null=False)
-	tipo_mov = models.ForeignKey(Tipo_mov, verbose_name='Tipo de Movimiento', on_delete=models.PROTECT, blank=False, null=False)
-	cod_empl = models.ForeignKey(Empleado, verbose_name='Empleado', on_delete=models.PROTECT, blank=False, null=False)
-	id_producto = models.ForeignKey(Producto, verbose_name='Producto', on_delete=models.PROTECT, blank=False, null=False)
-	cantidad = models.IntegerField(blank=False, null=False)
-
-	class Meta:
-		verbose_name = 'Movimiento de Inventario'
-		verbose_name_plural = 'Movimientos de Inventario'
-		ordering = ['fecha_mov']
-
-	def __str__(self):
-		return f'{self.fecha_mov} {self.tipo_mov}'
-
-	def save(self, *args, **kwargs):
-		self.motivo_mov = (self.motivo_mov).upper()
-		return super(Movimiento_inventario, self).save(*args, **kwargs)
 
 """
 # usuario base
--- apps/movimientos/models.py
+++ apps/movimientos/models.py
@@ -1,3 +1,89 @@
 from django.db import models
+from django.forms import model_to_dict
+
+from apps.entidades.models import Paciente, Empleado
+from apps.inventario.models import Producto, Inventario
 
 # Create your models here.
+# solicitud de producto
+class Solicitud(models.Model):
+	
+	class Status(models.TextChoices):
+		EN_PROCRESO = 'EP', 'En Proceso'
+		APROBADO = 'AP', 'Aprobado'
+		EN_ESPERA_DE_ENTREGA = 'EE', 'En Espera de Entrega'
+		ENTREGADO = 'ET', 'Entregado'
+
+	fecha_soli = models.DateField(auto_now=True, blank=False, null=False)
+	cod_pac = models.ForeignKey(Paciente, verbose_name='Paciente', on_delete=models.PROTECT, blank=False, null=False)
+	descripcion_soli = models.TextField(blank=False, null=False)
+	recipe = models.FileField(upload_to='recipes/', blank=False, null=False)
+	proceso_actual = models.CharField(max_length=50, default='ATENCION AL CLIENTE')
+	estado = models.CharField(max_length=2, choices=Status.choices, default=Status.EN_PROCRESO, blank=False, null=False)
+	validado_almacenista = models.BooleanField(default=False)
+	validado_admin = models.BooleanField(default=False)
+
+	class Meta:
+		verbose_name = 'Solicitud de Medicamento'
+		verbose_name_plural = 'Solicitud de Medicamentos'
+		ordering = ['fecha_soli']
+
+	def __str__(self):
+		return f'{self.descripcion_soli}'
+
+	def toJSON(self):
+		item = model_to_dict(self)
+		return item
+
+# detalle de la solicitud
+class DetalleSolicitud(models.Model):
+	solicitud = models.ForeignKey(Solicitud, on_delete=models.CASCADE)
+	producto = models.ForeignKey(Inventario, on_delete=models.CASCADE)
+	cantidad = models.IntegerField(default=1)
+
+	class Meta:
+		unique_together = ('solicitud', 'producto')  # Asegurar que no haya duplicados
+
+	def __str__(self):
+		return f'{self.solicitud.id_soli} - {self.producto.nombre} - {self.cantidad}'
+
+	def toJSON(self):
+		item = model_to_dict(self)
+		return item
+
+# tipo de movimiento
+class Tipo_mov(models.Model):
+	nombre = models.CharField(max_length=50)
+
+	class Meta:
+		verbose_name = 'Tipo de Movimiento'
+		verbose_name_plural = 'Tipos de Movimientos'
+		ordering = ['nombre']
+
+	def __str__(self):
+		return f'{self.nombre}'
+
+	def toJSON(self):
+		item = model_to_dict(self)
+		return item
+
+# movimiento del inventario
+# class Movimiento_inventario(models.Model):
+# 	fecha_mov = models.DateField(auto_now=True, blank=False, null=False)
+# 	motivo_mov = models.TextField(blank=False, null=False)
+# 	tipo_mov = models.ForeignKey(Tipo_mov, verbose_name='Tipo de Movimiento', on_delete=models.PROTECT, blank=False, null=False)
+# 	cod_empl = models.ForeignKey(Empleado, verbose_name='Empleado', on_delete=models.PROTECT, blank=False, null=False)
+# 	id_producto = models.ForeignKey(Producto, verbose_name='Producto', on_delete=models.PROTECT, blank=False, null=False)
+# 	cantidad = models.IntegerField(blank=False, null=False)
+
+# 	class Meta:
+# 		verbose_name = 'Movimiento de Inventario'
+# 		verbose_name_plural = 'Movimientos de Inventario'
+# 		ordering = ['fecha_mov']
+
+# 	def __str__(self):
+# 		return f'{self.fecha_mov} {self.tipo_mov}'
+
+# 	def save(self, *args, **kwargs):
+# 		self.motivo_mov = (self.motivo_mov).upper()
+# 		return super(Movimiento_inventario, self).save(*args, **kwargs)
