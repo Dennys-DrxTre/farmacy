ba293d9ff8b316d9b3ef97361717104cf1068851
dennys <dennyscorrea58@gmail.com>
2024-04-02 17:30:39 -0400

    correcion en config media url

--- CONFIG/urls.py
+++ CONFIG/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('apps.inventario.urls')),
     path('', include('apps.entidades.urls')),
     path('', include('apps.movimientos.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
